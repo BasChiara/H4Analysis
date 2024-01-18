7aed266b62a8824f7da6a7c2de23cc5161f01bb9
Chiara Basile <basilechiara@libero.it>
2024-01-18 10:00:38 +0100

    actual beam energy from simulation

---
+++ ECAL_TB_2021_analysis/utils/RealBeamEnergies.py
@@ -0,0 +1,32 @@
+# real beam energies 
+# from https://indico.cern.ch/event/1332535/contributions/5609745/attachments/2726695/4738855/ECAL_TestBeamUpdate_03Oct2023.pdf
+
+Ebeam_H4_value ={
+   20 :  19.96,
+   25 :  24.95, # my extrapolation 
+   50 :  49.90,
+   75 :  74.85, # my extrapolation 
+   80 :  79.84,
+   100:  99.71,
+   120: 119.47,
+   125: 124.63, # my extrapolation 
+   150: 148.86,
+   175: 171.49, # my extrapolation 
+   200: 195.99,
+   250: 240.78,
+}
+Ebeam_H4_error={
+   20 :   0.05,
+   25 :   0.06, # my extrapolation with unc 0.25%
+   50 :   0.13,
+   75 :   0.16, # my extrapolation with unc 0.20%
+   80 :   0.17, 
+   100:   0.19, 
+   120:   0.22, 
+   125:   0.22, # my extrapolation with unc 0.18%
+   150:   0.22,
+   175:   0.26, # my extrapolation with unc 0.15%
+   200:   0.33,
+   250:   0.48,
+
+}
