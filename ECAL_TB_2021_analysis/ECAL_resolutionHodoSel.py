bb2ca1cd5d55b43876db59e792b5224e90ee18f5
Chiara Basile <basilechiara@libero.it>
2024-01-18 10:01:40 +0100

    graphic changes

--- ECAL_TB_2021_analysis/ECAL_resolutionHodoSel.py
+++ ECAL_TB_2021_analysis/ECAL_resolutionHodoSel.py
@@ -38,21 +38,22 @@ dict_C2_energy   = {'25' : [15183], '50' : [15145, 15146], '75' : [15199], '100'
 LP_trees_path = '/eos/cms/store/group/dpg_ecal/comm_ecal/upgrade/testbeam/ECALTB_H4_Oct2021/LowPurity/ntuples_fitVFEs_fixes'
 LP_dict_energy_Nbins   = {'25' : 1000,   '50' : 800,   '75': 800,  '100': 800, '125': 800, '150': 800, '175' : 800, '200' : 400}
 LP_dict_energy_hodoX = {'25': 3.0, '50': 3.5, '75' : 2.0, '100': 2.5,'125': 1.5, '150' :  2.5, '175': 3.0, '200': 1.5}
-LP_dict_energy_hodoY = {'25':-6.0, '50':-7.0, '75' :-7.0, '100':-7.0,'125':-6.5, '150' : -6.5, '175':-7.0, '200':-7.0}
+LP_dict_energy_hodoY = {'25':-7.0, '50':-7.0, '75' :-7.0, '100':-7.0,'125':-6.5, '150' : -6.5, '175':-7.0, '200':-7.0}
 LP_crystal = 'C2'
 C2matrix_3x3 = 'B1,B2,B3,C3,C2,C1,D1,D3,D2'.split(',') 
 
 
 # HIGH PURITY energy scan #
-dict_C3_energy   = {'50' : [14907,14914,14937,14938], '100' : [14918], '150' : [14943,14934], '200': [14951], '250': [14820,14821]}
-dict_C3_energy = {'100' : [14918]}#, '150' : [14943], '200': [14951], '250': [14820,14821]}
+dict_C3_energy   = {'50' : [14907,14914,14932], '100' : [14918], '150' : [14943,14934], '200': [14951], '250': [14820,14821]}
+dict_C3_energy   = {'50' : [14932], '100' : [14918], '150' : [14943,14934], '200': [14951], '250': [14820,14821]}
 HP_trees_path = '/eos/cms/store/group/dpg_ecal/comm_ecal/upgrade/testbeam/ECALTB_H4_Oct2021/HighPurity/ntuples_fitVFEs_fixes/'
-HP_dict_energy_Nbins   = {'50' : 2400, '100': 800, '150': 800, '200' :800, '250' : 500}
-HP_dict_energy_hodoX = {'100': 3.5, '150':  3.0, '200': 2.5, '250': 1.5}
-HP_dict_energy_hodoY = {'100':-5.5, '150':-5.0, '200':-7.0, '250':-6.5}
+HP_dict_energy_Nbins   = {'50' : 800, '100': 800, '150': 800, '200' :500, '250' : 500}
+HP_dict_energy_hodoX = {'50' : -5.5, '100': 3.5, '150':  3.5, '200': 3.5, '250': 1.5}
+HP_dict_energy_hodoY = {'50' : -4.5, '100':-5.5, '150': -4.5, '200':-5.5, '250':-6.5}
 if(args.G1): 
-    dict_C3_energy   = {'100' : [15038]}#, '150': [14943,14934],'200': [14951], '250': [14820,14821]} # G1-forzato
-    HP_dict_energy_Nbins   = {'50' : 2400, '100': 150, '150': 1000, '200' :600, '250' : 800}
+    dict_C3_energy['100'] = [15038]#, '150': [14943,14934],'200': [14951], '250': [14820,14821]} # G1-forzato
+    HP_dict_energy_Nbins['200'] = 500
+    HP_dict_energy_Nbins['100'] = 400
 HP_crystal = 'C3'
 C3matrix_3x3 = 'B4,B2,B3,C3,C2,C4,D4,D3,D2'.split(',') 
 
@@ -132,52 +133,56 @@ for energy in energies :
 
     myCB.set_crystal(crystal)
     myCB.set_energy(energy)
-    myCB.set_position(HodoX[energy], HodoY[energy], 2.0) 
-    myCB.xaxis_scale = 0.1
+    myCB.set_position(HodoX[energy], HodoY[energy], 2.0 if (args.mode == 'HP') else 1.5) 
 
 
     # initial fit-parameters
     ## LOW PURITY ##
     if(args.mode == 'LP'):
+        myCB.xaxis_scale = 0.15
+        if(args.G1): myCB.gain = 10
         myCB.a_min = 0.1; myCB.a_max = 5.; myCB.a_initial = 1.5
         myCB.a2_min = 0.1; myCB.a2_max = 5; myCB.a2_initial = 1.5
         myCB.n_min = 0.1; myCB.n_max = 50; myCB.n_initial = 10 
         myCB.n2_min = 0.1; myCB.n2_max = 50; myCB.n2_initial = 10 
         if (E>25):
+            myCB.xaxis_scale = 0.1
             myCB.a_min = 0.1; myCB.a2_max = 10.; myCB.a2_initial = 1.5
             myCB.a2_min = 0.1; myCB.a2_max = 10.; myCB.a2_initial = 1.5
             myCB.n_min = 0.1; myCB.n_max = 10; myCB.n_initial = 1
             myCB.n2_min = 0.01; myCB.n2_max = 10; myCB.n2_initial = 5
         if (E>50): 
-            myCB.n_min = 0.1; myCB.n_max = 5; myCB.n_initial = 1
-            myCB.n2_min =0.5; myCB.n2_max = 5; myCB.n2_initial =1 
-            myCB.xaxis_scale = 0.1
+            myCB.n_min  = 0.1; myCB.n_max = 5; myCB.n_initial = 1
+            myCB.n2_min = 0.5; myCB.n2_max = 5; myCB.n2_initial =1 
         if (E>75): 
+            myCB.xaxis_scale = 0.075
             myCB.n_min = 1; myCB.n_max = 10; myCB.n_initial = 5 
-            myCB.xaxis_scale = 0.07
         if (E>100): 
+            myCB.xaxis_scale = 0.070
             myCB.n_min =  1; myCB.n_max = 200; myCB.n_initial = 10 
             myCB.n2_min = 1; myCB.n2_max = 150; myCB.n2_initial = 10 
         if(E>150): 
             if(args.gain_switch): myCB.gain = 10
             myCB.n_min =  0.1; myCB.n_max = 20; myCB.n_initial = 5 
-            myCB.n2_min = 0.1; myCB.n2_max = 15; myCB.n2_initial = 5 
-            myCB.a_min = 0.05; myCB.a2_max = 5.; myCB.a2_initial = 1.5
+            myCB.n2_min = 0.01; myCB.n2_max = 15; myCB.n2_initial = 5 
+            myCB.a_min = 0.05; myCB.a2_max = 10.; myCB.a2_initial = 1.5
     ## HIGH PURITY ###
     if(args.mode == 'HP'):
         myCB.gain = 1
-        myCB.xaxis_scale = 0.10
+        if(args.G1 and E < 200):continue
         if(args.G1): myCB.gain = 10
+        myCB.xaxis_scale = 0.10
         myCB.n_min = 0.1; myCB.n_max = 50; myCB.n_initial = 1. 
         myCB.n2_min = 0.1; myCB.n2_max = 50; myCB.n2_initial = 1. 
         myCB.a2_max = 0.1; myCB.a2_max = 10; myCB.a2_initial = 1.5
         myCB.a_min = 0.1; myCB.a_max = 10; myCB.a_initial = 0.5 
-        #if (E>100): 
+        if (E>100): 
+            myCB.xaxis_scale = 0.05
             #myCB.n2_min = 1; myCB.n2_max = 100; myCB.n2_initial = 10 
             #myCB.n_min = 1; myCB.n_max = 50; myCB.n_initial = 10 
         if (E>150): 
             if(args.gain_switch): myCB.gain = 10
-            myCB.n2_min = 0.01; myCB.n2_max = 1.; myCB.n2_initial = .5 
+            myCB.n2_min = 0.01; myCB.n2_max = 10.; myCB.n2_initial = .5 
         if (E>200): 
             myCB.n_min = 0.1; myCB.n_max = 50; myCB.n_initial = 12 
         
@@ -197,12 +202,14 @@ for energy in energies :
     MX_results.append(tmp_dict)
 
     canvas_num+=1
+    print("  canvas slot : ",canvas_num)
         
-    if(canvas_num % 4 == 0 or canvas_num == len(energies)):
+    if(canvas_num % 4 == 0 or energy == energies[-1]):
        n_canvas += 1
        c.Draw()
        c.SaveAs('%s/Intercalib_TemplFits_%s_%s_%d.png'%(plot_folder,outstr,crystal,n_canvas))
        c.SaveAs('%s/Intercalib_TemplFits_%s_%s_%d.pdf'%(plot_folder,outstr,crystal,n_canvas))
+       c.Clear("D")
        canvas_num = 0
 
 ############## SAVE RESULTS ##############
@@ -211,4 +218,4 @@ with open("results_hodo/%sresults_%s.json"%(crystal,outstr), "w") as fp:
     json.dump(MX_results,fp) 
 
 
-print(' [OUT] fit parameters saved in results/%sresults_%s.json'%(crystal,outstr))
+print(' [OUT] fit parameters saved in results_hodo/%sresults_%s.json'%(crystal,outstr))
--- ECAL_TB_2021_analysis/plot_maker.py
+++ ECAL_TB_2021_analysis/plot_maker.py
@@ -9,6 +9,9 @@ from scipy.optimize import curve_fit
 import os
 from uncertainties import unumpy
 from uncertainties import ufloat
+import sys
+sys.path.insert(0, 'utils/')
+import RealBeamEnergies as RealE
 
 
 import argparse
@@ -36,7 +39,9 @@ parser.add_argument('-i', '--input', help = 'JSON file to read in input')
 parser.add_argument('-o', '--output', help = 'folder to save plots', default = '/eos/user/c/cbasile/www/ECAL_TB2021/Linearity/FitAmpl')
 parser.add_argument('--tag', default = 'C2')
 parser.add_argument('--crystal', default= 'C2 crystal')
-parser.add_argument('--gain_compare', action = 'store_true')
+parser.add_argument('--free_params', action='store_true')
+parser.add_argument('--fix_S', action='store_true')
+parser.add_argument('--gain_compare')#, action = 'store_true')
 
 args = parser.parse_args ()
 plot_folder = args.output
@@ -55,6 +60,7 @@ CBsigma_err = []
 sigma_over_mean_C2 = []
 energies_float_unc = []
 for item in results:
+    if ( (float)(list(item.keys())[0]) >= 250 ) : continue
     energies_float.append((float)(list(item.keys())[0]))
     energies_float_unc.append((float)(list(item.keys())[0])*0.005)
     CBmeans.append(item[list(item.keys())[0]]['CBmean'][0])
@@ -64,15 +70,14 @@ for item in results:
 
 print(f' -> E [GeV] : {energies_float}')
 
-
-means =  unumpy.umatrix(CBmeans, CBmeans_err)
+real_energies_val = np.asarray([RealE.Ebeam_H4_value[e] for e in energies_float if e < 250], dtype=float)
+real_energies_err = np.asarray([RealE.Ebeam_H4_error[e] for e in energies_float if e < 250], dtype=float)
+E_beam = unumpy.umatrix(real_energies_val, real_energies_err)
+means  =  unumpy.umatrix(CBmeans, CBmeans_err)
 sigmas =  unumpy.umatrix(CBsigma, CBsigma_err)
 
 sigma_over_mean = sigmas/means
 
-
-
-############## --------------- LINEARITY FIT --------------- ##############
 
 ### ECAL text
 ECALtex = ROOT.TLatex()
@@ -82,9 +87,12 @@ ECALtex.SetTextColor(ROOT.kBlack)
 ECALtex.SetTextSize(0.06)
 ECALtex.SetTextAlign(12)
 
+
+############## --------------- LINEARITY FIT --------------- ##############
 def linear_func(x, a, b):
     return a * x + b
-popt, pcov = curve_fit(linear_func, energies_float, CBmeans, sigma=CBmeans_err,absolute_sigma=True)
+
+popt, pcov = curve_fit(linear_func, real_energies_val, CBmeans, sigma=CBmeans_err,absolute_sigma=True)
 perr = np.sqrt(np.diag(pcov))
 covariance = pcov[0,1]
 print('fit parametes and 1-sigma errors:')
@@ -92,30 +100,29 @@ for i in range(len(popt)):
     print('\t par[%d] = %.3f +- %.3f'%(i,popt[i],perr[i]))
 print('\t covariance = %.3f '%(covariance))
 
-fit_CBmeans = linear_func(np.asarray(energies_float), popt[0], popt[1])
-fit_CBmeans_err = np.sqrt(perr[1]**2 + (perr[0]*np.asarray(energies_float))**2)
+fit_CBmeans = linear_func(np.asarray(real_energies_val), popt[0], popt[1])
+fit_CBmeans_err = np.sqrt(perr[1]**2 + (perr[0]*real_energies_val)**2 + (popt[0]*real_energies_err)**2)
 fit_means = unumpy.umatrix(fit_CBmeans, fit_CBmeans_err)
-#print(means)
-#print(fit_means)
-#residuals = (CBmeans - linear_func(np.asarray(energies_float), popt[0], popt[1]))/linear_func(np.asarray(energies_float), popt[0], popt[1])
 residuals = (means - fit_means)/fit_means
 pull = (means -fit_means)/CBmeans_err
-#print(residuals)
-xfine = np.linspace(0., 200, 1000)  # define values to plot the function for
+print(" [=] lin-fit residuals : ", residuals)
 
 c0 = ROOT.TCanvas("C2_linearity", "", 900, 1024)
+Xmin = 0; Xmax = 260# GeV
+Ymin = 0.; Ymax = 7000
+
 up_pad = ROOT.TPad("up_pad", "", 0., 0.30, 1.,1.) 
 up_pad.Draw()
 up_pad.SetMargin(0.15, 0.1,0.0,0.1)
 up_pad.SetGrid()
 up_pad.cd()
-lin_gr = ROOT.TGraphErrors(len(energies_float), np.array(energies_float).astype("float"),np.array(CBmeans).astype("float"),np.array(energies_float_unc).astype("float") ,np.array(CBmeans_err).astype("float"))
-fit_func = ROOT.TF1("lin_func", "[0]*x + [1]", 10, 300)
+lin_gr = ROOT.TGraphErrors(len(real_energies_val), np.array(real_energies_val).astype("float"),np.array(CBmeans).astype("float"),np.array(real_energies_err).astype("float") ,np.array(CBmeans_err).astype("float"))
+fit_func = ROOT.TF1("lin_func", "[0]*x + [1]", Xmin, Xmax)
 fit_func.SetParameter(0, popt[0]); fit_func.SetParameter(1, popt[1])
 fit_func.SetLineWidth(3)
 lin_gr.SetMarkerStyle(20)
-#FitRes = lin_gr.Fit(linear_func, "FS", "L", 20, 200)
-#print(f' = Linear-fit Chi2 = {FitRes.Chi2}')
+lin_gr.GetYaxis().SetRangeUser(Ymin,Ymax)
+lin_gr.GetXaxis().SetLimits(Xmin, Xmax)
 lin_gr.SetTitle("")
 lin_gr.GetYaxis().SetTitle("Reconstructed energy (ADC count)"); lin_gr.GetYaxis().SetTitleSize(0.05)
 lin_gr.GetYaxis().SetLabelSize(0.04); 
@@ -133,14 +140,15 @@ ratio_pad.Draw()
 ratio_pad.cd()
 ratio_pad.SetGrid()
 ratio_pad.Draw()
-print(type(energies_float))
-res_gr= ROOT.TGraphErrors(len(energies_float),np.array(energies_float, dtype= float),unumpy.nominal_values(residuals)*100,np.array(energies_float_unc, dtype= float),unumpy.std_devs(residuals)*100) 
-#res_gr= ROOT.TGraphErrors(len(energies_float),np.array(energies_float, dtype= float),unumpy.nominal_values(pull),np.array(energies_float_unc, dtype= float), np.ones(len(energies_float)))
+print(type(real_energies_val))
+res_gr= ROOT.TGraphErrors(len(real_energies_val),np.array(real_energies_val, dtype= float),unumpy.nominal_values(residuals)*100,np.array(real_energies_err, dtype= float),unumpy.std_devs(residuals)*100) 
+#res_gr= ROOT.TGraphErrors(len(real_energies_val),np.array(real_energies_val, dtype= float),unumpy.nominal_values(pull),np.array(real_energies_err, dtype= float), np.ones(len(real_energies_val)))
 zero_level = ROOT.TLine(lin_gr.GetXaxis().GetXmin(),0, lin_gr.GetXaxis().GetXmax(),0); zero_level.SetLineWidth(3); zero_level.SetLineColor(ROOT.kRed)
 res_gr.SetTitle("")
 res_gr.GetXaxis().SetTitle("Beam energy (GeV)"); res_gr.GetXaxis().SetTitleSize(0.1); res_gr.GetXaxis().SetTitleOffset(1)
 res_gr.GetXaxis().SetLabelSize(0.1)
-res_gr.GetYaxis().SetRangeUser(-5,5)
+res_gr.GetYaxis().SetRangeUser(-1.0,1.0)
+res_gr.GetXaxis().SetLimits(Xmin, Xmax)
 res_gr.GetYaxis().SetTitle("(E_{reco}-E_{reco}^{fit})/E_{reco}^{fit}(%)"); res_gr.GetYaxis().CenterTitle(); res_gr.GetYaxis().SetTitleSize(0.1); res_gr.GetYaxis().SetTitleOffset(0.65)
 #res_gr.GetYaxis().SetTitle("Pull")
 res_gr.GetYaxis().SetLabelSize(0.1); res_gr.GetYaxis().SetNdivisions(-504)
@@ -159,41 +167,82 @@ c0.SaveAs('%s/ADC_to_GeV_%s.pdf'%(plot_folder,args.tag))
     
 
 c1 = ROOT.TCanvas("C2_resolution", "", 800, 800)
+Ymin = 0.; Ymax = 0.035
+Xmin = 0; Xmax = 300 # GeV
 
-ROOT.gStyle.SetLabelFont(42);
+ROOT.gStyle.SetLabelFont(42)
     
 y=array.array('d',unumpy.nominal_values(sigma_over_mean).tolist()[0])
 ey=array.array('d',unumpy.std_devs(sigma_over_mean).tolist()[0])
-
+ey[0] = y[0]*0.025
+print(' [>] resolution errors', ey)
+N_fixed = 0.472 # noise + correlations
+nominal_S = 0.025
 # resolution fit
-def resol_func(x, S, C):
-    N = 0.37 # fixed noise term 
+def resol_func(x, N, S, C):
+    #N = 0.55 # fixed noise term 
     resol = N*N/(x*x) + S*S/x + C*C
     return np.sqrt(resol)
-popt, pcov = curve_fit(resol_func, energies_float, y, sigma=ey,absolute_sigma=True, bounds= ([0.001, 0.], [0.1, 0.01]))
+
+if (args.free_params):
+   print("[FIT] all the parameters are free")
+   popt, pcov = curve_fit(resol_func, 
+                           real_energies_val, 
+                           y, 
+                           sigma=ey,
+                           absolute_sigma=True,
+                           p0=(0.50, 0.03, 0.005),
+                           bounds= ([0.3, 0.001, 0.], [1.0 , 0.1, 0.01]))
+elif (args.fix_S):
+   print("[FIT] with S fixed")
+   popt, pcov = curve_fit(lambda x, N, C : resol_func(x, N, nominal_S, C), 
+                           real_energies_val, 
+                           y, 
+                           sigma=ey,
+                           absolute_sigma=True, 
+                           p0=(0.5, 0.005),
+                           bounds= ([0.30, 0.001], [1.0, 0.01]))
+else:
+   print("[FIT] with N fixed")
+   popt, pcov = curve_fit(lambda x, S, C : resol_func(x, N_fixed, S, C), 
+                           real_energies_val, 
+                           y, 
+                           sigma=ey,
+                           absolute_sigma=True, 
+                           p0=(0.03, 0.005),
+                           bounds= ([0.0241, 0.001], [0.1, 0.01]))
+
 perr = np.sqrt(np.diag(pcov))
 print('fit parametes and 1-sigma errors:')
 for i in range(len(popt)):
     print('\t par[%d] = %.3f +- %.3f'%(i,popt[i],perr[i]))
 
-fit_resol = ROOT.TF1("resol_func", "sqrt(0.37*0.37/(x*x) + [0]*[0]/x + [1]*[1])", 10, 300)
-fit_resol.SetParameter(0, popt[0]); fit_resol.SetParameter(1, popt[1]); 
+fit_resol = ROOT.TF1("resol_func", "sqrt([0]*[0]/(x*x) + [1]*[1]/x + [2]*[2])", 0, 300)
+if (args.free_params): 
+   fit_resol.SetParameter(0, popt[0]); 
+   fit_resol.SetParameter(1, popt[1]); 
+   fit_resol.SetParameter(2, popt[2]); 
+elif (args.fix_S):
+   fit_resol.SetParameter(0, popt[0]); 
+   fit_resol.SetParameter(1, nominal_S); 
+   fit_resol.SetParameter(2, popt[1]); 
+else:
+   fit_resol.SetParameter(0, N_fixed); 
+   fit_resol.SetParameter(1, popt[0]); 
+   fit_resol.SetParameter(2, popt[1]); 
 fit_resol.SetLineWidth(3)
 
 
 # make plot
 
-gr = ROOT.TGraphErrors(len(energies_float),array.array('d',energies_float),y,array.array('d',energies_float_unc) ,ey )
+gr = ROOT.TGraphErrors(len(real_energies_val),array.array('d',real_energies_val),y,array.array('d',real_energies_err) ,ey )
 gr.SetMarkerStyle(20)
 
 gr.SetTitle('')
 gr.GetYaxis().SetTitleSize(0.04)
 gr.GetYaxis().SetLabelSize(0.04)
-Ymin = 0.; Ymax = 0.035
 gr.GetYaxis().SetRangeUser(Ymin, Ymax)
-Xmin = 0.; Xmax = 300 
 gr.GetXaxis().SetLimits(Xmin, Xmax)
-#gr.GetYaxis().SetNdivisions(-510)
 gr.GetYaxis().SetTitleOffset(1.9)
 gr.GetYaxis().SetTitle( '#sigma(E)/E')
 gr.GetXaxis().SetTitleOffset(1.5)
@@ -202,34 +251,44 @@ gr.GetXaxis().SetLabelSize(0.04)
 gr.GetXaxis().SetTitle( 'E (GeV)' )
 gr.Draw( 'AP' )
 fit_resol.Draw('same')
+gr.Draw( 'P' )
 # legend
-leg = ROOT.TLegend(0.5,0.7,0.8,0.9)
+leg = ROOT.TLegend(0.40,0.68,0.8,0.88)
 leg.SetFillStyle(-1)
 leg.SetBorderSize(0)
-leg.SetTextFont(42)
+leg.SetTextFont(62)
 leg.SetTextSize(0.035)
-leg.AddEntry(gr,f'ECAL {args.crystal}','P')
+leg.AddEntry(gr,f'ECAL {args.crystal} high-gain','P')
 leg.AddEntry(fit_resol, "#frac{N}{E} #oplus #frac{S}{#sqrt{E}} #oplus C", "l")
 leg.Draw()
 # text on plot
 fit_txt = ROOT.TLatex()
-fit_txt.SetTextFont(42)
+fit_txt.SetTextFont(62)
 fit_txt.SetTextAngle(0)
 fit_txt.SetTextColor(ROOT.kBlack)    
-fit_txt.SetTextSize(0.035)    
+fit_txt.SetTextSize(0.035)
 fit_txt.SetTextAlign(12)
-X_fit_txt = 0.45*gr.GetXaxis().GetXmax(); Y_fit_txt = 0.65*Ymax; dY_fit_txt = 0.002
+X_fit_txt = 0.40*gr.GetXaxis().GetXmax(); Y_fit_txt = 0.65*Ymax; dY_fit_txt = 0.002
 #fit_txt.DrawLatex(X_fit_txt, Y_fit_txt,             'N = %.4f #pm %.4f'%(popt[0], perr[0]))
-fit_txt.DrawLatex(X_fit_txt, Y_fit_txt,             'N = 0.34 GeV (fixed)')
-fit_txt.DrawLatex(X_fit_txt, Y_fit_txt-dY_fit_txt,  'S = (%.4f #pm %.4f) GeV^{1/2}'%(np.abs(popt[0]), perr[0]))
-fit_txt.DrawLatex(X_fit_txt, Y_fit_txt-2*dY_fit_txt,'C = (%.4f #pm %.4f)'%(popt[1], perr[1]))
+if (args.free_params):
+   fit_txt.DrawLatex(X_fit_txt, Y_fit_txt,             'N = (%.4f #pm %.4f) GeV'%(np.abs(popt[0]), perr[0]))
+   fit_txt.DrawLatex(X_fit_txt, Y_fit_txt-dY_fit_txt,  'S = (%.4f #pm %.4f) GeV^{1/2}'%(np.abs(popt[1]), perr[1]))
+   fit_txt.DrawLatex(X_fit_txt, Y_fit_txt-2*dY_fit_txt,'C = (%.4f #pm %.4f)'%(popt[2], perr[2]))
+elif (args.fix_S):
+   fit_txt.DrawLatex(X_fit_txt, Y_fit_txt,             'N = (%.4f #pm %.4f) GeV'%(np.abs(popt[0]), perr[0]))
+   fit_txt.DrawLatex(X_fit_txt, Y_fit_txt-dY_fit_txt,  'S = %.3f GeV^{1/2} (fixed)'%nominal_S)
+   fit_txt.DrawLatex(X_fit_txt, Y_fit_txt-2*dY_fit_txt,'C = (%.4f #pm %.4f)'%(popt[1], perr[1]))
+else:
+   fit_txt.DrawLatex(X_fit_txt, Y_fit_txt,             'N = %.2f GeV (fixed)' %N_fixed)
+   fit_txt.DrawLatex(X_fit_txt, Y_fit_txt-dY_fit_txt,  'S = (%.4f #pm %.4f) GeV^{1/2}'%(np.abs(popt[0]), perr[0]))
+   fit_txt.DrawLatex(X_fit_txt, Y_fit_txt-2*dY_fit_txt,'C = (%.4f #pm %.4f)'%(popt[1], perr[1]))
 ECALtex.SetTextSize(0.045)
 ECALtex.DrawLatex(20,0.005, "#bf{ECAL} Test Beam 2021")
 
 
 ## superimpose gain-switched points ##
 if (args.gain_compare):
-    input2 = './results/C3results_HP_'+ args.crystal +'_gain1.json'
+    input2 = args.gain_compare 
     print(' >> gain comparison input file : %s' %input2)
     with open(input2, 'r') as openfile2:
         # Reading from json file
@@ -254,13 +313,16 @@ if (args.gain_compare):
     print(f' -> gain 1 : E [GeV] : {G_energies_float}')
     
     
+    Greal_energies_val = np.asarray([RealE.Ebeam_H4_value[e] for e in G_energies_float], dtype=float)
+    Greal_energies_err = np.asarray([RealE.Ebeam_H4_error[e] for e in G_energies_float], dtype=float)
+    GE_beam = unumpy.umatrix(real_energies_val, real_energies_err)
     Gmeans =  unumpy.umatrix(G_CBmeans, G_CBmeans_err)
     Gsigmas =  unumpy.umatrix(G_CBsigma, G_CBsigma_err)
     
     Gsigma_over_mean = Gsigmas/Gmeans
     
-    Ggr = ROOT.TGraphErrors(len(G_energies_float),array.array('d',G_energies_float),
-                            array.array('d',unumpy.nominal_values(Gsigma_over_mean).tolist()[0]),array.array('d',G_energies_float_unc) ,
+    Ggr = ROOT.TGraphErrors(len(Greal_energies_val),array.array('d',Greal_energies_val),
+                            array.array('d',unumpy.nominal_values(Gsigma_over_mean).tolist()[0]),array.array('d',Greal_energies_err) ,
                             array.array('d',unumpy.std_devs(Gsigma_over_mean).tolist()[0]) )
 
     # resolution curve
@@ -268,32 +330,37 @@ if (args.gain_compare):
 
     y=array.array('d',unumpy.nominal_values(Gsigma_over_mean).tolist()[0])
     ey=array.array('d',unumpy.std_devs(Gsigma_over_mean).tolist()[0])
+    N_fixed_G1 = N_fixed * 10.14/10.
     def resol_func_G1(x, S, C):
-        N = 0.34*np.sqrt(10.14) # fixed noise term 
+        N = N_fixed_G1 
         resol = N*N/(x*x) + S*S/x + C*C
         return np.sqrt(resol)
     Gpopt, Gpcov = curve_fit(resol_func_G1, G_energies_float, y, sigma=ey,absolute_sigma=True, bounds= ([0.00005, 0.005], [1., 0.006]))
     Gperr = np.sqrt(np.diag(Gpcov))
     print('G1 fit parametes and 1-sigma errors:')
     for i in range(len(Gpopt)):
+
         print('\t par[%d] = %.3f +- %.3f'%(i,Gpopt[i],Gperr[i]))
 
-    fit_resol_G1 = ROOT.TF1("resol_func_G1", "sqrt((1.08*1.08)/(x*x) + [0]*[0]/x + [1]*[1])", 10, 300)
-    fit_resol_G1.SetParameter(0, Gpopt[0]); fit_resol_G1.SetParameter(1, Gpopt[1]); #fit_resol_G1.SetParameter(2, Gpopt[2]); 
+    fit_resol_G1 = ROOT.TF1("resol_func_G1", "sqrt([0]*[0]/(x*x) + [1]*[1]/x + [2]*[2])", 10, 300)
+    #fit_resol_G1.SetParameter(0, N_fixed_G1); fit_resol_G1.SetParameter(1, Gpopt[0]); fit_resol_G1.SetParameter(2, Gpopt[1]); 
+    fit_resol_G1.SetParameter(0, N_fixed_G1); fit_resol_G1.SetParameter(1, popt[0]); fit_resol_G1.SetParameter(2, popt[1]); 
     fit_resol_G1.SetLineWidth(3)
     fit_resol_G1.SetLineColor(ROOT.kGray)
 
 
     Ggr.SetMarkerStyle(20)
     Ggr.SetMarkerColor(ROOT.kBlue)
-    leg.AddEntry(Ggr,f'ECAL {args.crystal} - gain = 1','P')
+    leg.AddEntry(Ggr,f'ECAL {args.crystal} low-gain','P')
+    #fit_resol_G1.Draw('same')
     Ggr.Draw( 'P' )
-    fit_resol_G1.Draw('same')
     
 
-c1.Draw()  
+#c1.Draw()  
 ROOT.gStyle.SetLineWidth(2)
 ROOT.gPad.SetMargin(0.15,0.10,0.15, 0.10)
 ROOT.gPad.SetGridx(1); ROOT.gPad.SetGridy(1)
-c1.SaveAs('%s/EnergyResolution_C2_%s.pdf'%(plot_folder,args.tag))
-c1.SaveAs('%s/EnergyResolution_C2_%s.png'%(plot_folder,args.tag))
+ROOT.gPad.RedrawAxis()
+c1.Update()  
+c1.SaveAs('%s/EnergyResolution_%s_%s.pdf'%(plot_folder, args.crystal ,args.tag))
+c1.SaveAs('%s/EnergyResolution_%s_%s.png'%(plot_folder, args.crystal, args.tag))
